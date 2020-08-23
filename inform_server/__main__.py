aab6681cab3001f83952acf796061932f751a464
Victor Wang <victorwang2001@gmail.com>
2020-08-23 16:24:31 -0400

    add app

--- inform_server/__main__.py
+++ inform_server/__main__.py
@@ -6,13 +6,10 @@ from inform_server import encoder
 
 from flask_cors import CORS
 
-def main():
+if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Inform API'}, pythonic_params=True)
     CORS(app.app)
     app.run(port=8080, debug=True)
 
-
-if __name__ == '__main__':
-    main()
